12dcccc6a17617800b2fd13a64272b91ade7849d
Akari000 <app.sub.k@gmail.com>
2019-07-11 14:28:57 +0900

    fix number

--- chapter6.py
+++ chapter6.py
@@ -3,11 +3,11 @@
 import smbus
 import time
 import datetime  #課題2用
-import json  #課題3用
+import json  #課題2用
 
 i2c = smbus.SMBus(1)
 address = 0x48
-json_file = open('result.json', 'w')  #課題3用
+json_file = open('result.json', 'w')  #課題2用
 
 for i in range(10):
     block = i2c.read_i2c_block_data(address, 0x00, 12)
