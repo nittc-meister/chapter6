67f04b0737b8cd081123dc61edc5e2533568d225
SuzukiSoichiro <SuzukiSoichiro363636@gmail.com>
2019-07-11 17:10:26 +0900

    chenge chapter.py

--- chapter6.py
+++ chapter6.py
@@ -8,6 +8,7 @@ import json  #課題2用
 i2c = smbus.SMBus(1)
 address = 0x48
 json_file = open('result.json', 'w')  #課題2用
+now = datetime.datetime.now()
 
 for i in range(10):
     block = i2c.read_i2c_block_data(address, 0x00, 12)
@@ -16,5 +17,8 @@ for i in range(10):
         temp -= 8192
     print("Temperature:%6.2f" % (temp / 16.0))
     time.sleep(1)
+
+    dic = {"id" + str(i): {"time": str(now), "temp": temp}}
+    json.dump(dic, json_file, ensure_ascii=False, indent=4, sort_keys=True, separators=(',', ': '))
 
 json_file.close()
